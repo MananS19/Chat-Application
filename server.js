7c3e49b4a64d0974e234c7a7caa16ce8b0547bc4
Atharva Ranade <atharvajv@gmail.com>
2021-12-01 18:30:47 +0530

    Data Consistency

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var mongoose = require('mongoose');
+const axios = require('axios');
 
-app.use(express.static(__dirname));
+// app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 
@@ -30,6 +31,18 @@ app.get('/messages', (req, res) => {
 
 app.post('/messages', (req, res) => {
   var message = new Message(req.body);
+  axios
+  .post('http://localhost:3001/communicate', req.body);
+  message.save((err) =>{
+    if(err)
+      sendStatus(500);
+    io.emit('message', req.body);
+    res.sendStatus(200);
+  })
+})
+
+app.post('/communicate', (req, res) => {
+  var message = new Message(req.body);
   message.save((err) =>{
     if(err)
       sendStatus(500);
@@ -44,6 +57,10 @@ io.on('connection', () =>{
 
 mongoose.connect(dbUrl ,(err) => {
   console.log('mongodb connected',err);
+})
+
+app.get("/", (req, res)=>{
+  res.sendFile(__dirname + "/index.html");
 })
 
 var server = http.listen(3000, () => {
